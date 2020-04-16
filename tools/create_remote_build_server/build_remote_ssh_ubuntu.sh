6920746c872236821b6c86eca959979a7c894d44
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-04-16 12:50:26 +0200

    done

--- tools/create_remote_build_server/build_remote_ssh_ubuntu.sh
+++ tools/create_remote_build_server/build_remote_ssh_ubuntu.sh
@@ -9,4 +9,5 @@ MY_PACKER_TEMPLATES_GITHUB_REPOSITORY="https://github.com/ruzickap/packer-templa
 GITHUB_SELF_HOSTED_RUNNER_TOKEN=${GITHUB_SELF_HOSTED_RUNNER_TOKEN:-Axxxxxxxxxxxxxxxxxxxxxxxxxxx4}
 
 # Prepare remote machine for build
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i "$REMOTE_IP," -e "ansible_sudo_pass=$USER_PASSWORD" -e GITHUB_SELF_HOSTED_RUNNER_TOKEN="$GITHUB_SELF_HOSTED_RUNNER_TOKEN" -e MY_PACKER_TEMPLATES_GITHUB_REPOSITORY="$MY_PACKER_TEMPLATES_GITHUB_REPOSITORY" --user "$REMOTE_USER" build_remote_ssh_ubuntu.yml
