4ccd2adb12b8d6ed76486cba09b769ac0542f8fc
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-05-23 18:44:20 +0200

    bbb

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,17 @@ export EXCLUDE="${INPUT_EXCLUDE}"
 
 echo "111 $EXCLUDE"
 
-echo "$EXCLUDE" | xxd
-
 IFS="
 "
 
 FIND_EXCLUDE="-not -path ./node_modules/*"
-for i in "${EXCLUDE[@]}"; do
+for i in ${EXCLUDE[*]}; do
   echo "bbb ${i}"
   FIND_EXCLUDE+=('-not' '-path' "${i}")
 done
+
+exit
+
 
 FILE_EXTENSION="*.sh"
 
@@ -27,7 +28,6 @@ echo "aaa ${FIND_CALL[*]}"
 
 "${FIND_CALL[@]}"
 
-exit
 
 # Command line parameters for muffet
 export CMD_PARAMS="${INPUT_CMD_PARAMS:---buffer-size=8192 --concurrency=10}"
