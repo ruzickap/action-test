e943a874877fa28628358f82e03cbf066ff19bd6
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-07-01 10:52:35 +0200

    8

--- run_ansible_macos_workstation.sh
+++ run_ansible_macos_workstation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --diff -e ansible_python_interpreter=/usr/local/bin/python3 -e homebrew_casks=iterm2 -e homebrew="dockutil,fzf,romkatv/powerlevel10k/powerlevel10k" --connection=local -i "127.0.0.1," main.yml
+ansible-playbook --extra-vars "homebrew_casks=iterm2 -e homebrew=dockutil,fzf,romkatv/powerlevel10k/powerlevel10k" --connection=local -i "127.0.0.1," main.yml
