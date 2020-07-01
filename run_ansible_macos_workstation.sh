9e012b7c10bb338ef162bc16f809da93de0fbdae
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-07-01 09:31:31 +0200

    4

--- run_ansible_macos_workstation.sh
+++ run_ansible_macos_workstation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --diff -e ansible_python_interpreter=$(which python3) --connection=local -i "127.0.0.1," main.yml
+ansible-playbook --diff -e ansible_python_interpreter=/usr/local/bin/python3 --connection=local -i "127.0.0.1," main.yml
